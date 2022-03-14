608eb2e8e57d8aefb33140bbddadd81197728329
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2022-03-14 09:49:51 +0900

    Add files via upload

--- OpenCV01/Form1.Designer.cs
+++ OpenCV01/Form1.Designer.cs
@@ -29,10 +29,7 @@ namespace OpenCV01
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,27 +42,11 @@ namespace OpenCV01
             this.pictureBoxIpl1.TabIndex = 0;
             this.pictureBoxIpl1.TabStop = false;
             // 
-            // timer1
-            // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 33;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(664, 501);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBoxIpl1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -73,15 +54,12 @@ namespace OpenCV01
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Label label1;
     }
 }
 
--- OpenCV01/Form1.cs
+++ OpenCV01/Form1.cs
@@ -11,51 +11,14 @@ namespace OpenCV01
             InitializeComponent();
         }
 
-        CvCapture capture;
         IplImage src;
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            try
-            {
-                // 파일을 불러 오는 코드
-                capture = CvCapture.FromFile("../../../Images/Saigon.mp4");
-
-                // 카메라를 불러 오는 코드
-                /*
-                capture = CvCapture.FromCamera(CaptureDevice.DShow, 0); // 외부 카메라 사용시 index = 1
-                capture.SetCaptureProperty(CaptureProperty.FrameWidth, 640);
-                capture.SetCaptureProperty(CaptureProperty.FrameHeight, 480);
-                */
-                
-            }
-            catch
-            {
-                timer1.Enabled = false;
-            }
+            src = new IplImage("../../../Images/Italia.jpg");
+            pictureBoxIpl1.ImageIpl = src;
         }
 
-        int frameCount = 0;
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            frameCount++;
-            label1.Text = frameCount.ToString() + "/" + capture.FrameCount.ToString();
-            src = capture.QueryFrame();
-
-            if (frameCount != capture.FrameCount)
-            {
-                pictureBoxIpl1.ImageIpl = src;
-            }
-            else
-            {
-                frameCount = 0;
-                pictureBoxIpl1.ImageIpl = null;
-                timer1.Enabled = false;
-                // capture = CvCapture.FromFile("../../../Images/Saigon.mp4"); 무한 영상 재생
-            }
-
-        }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
